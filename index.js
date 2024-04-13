e9c3905454c9a926a1cae98ffd89f3791fa22df8
Rashid-Faheem <mrashidfaheem@gmail.com>
2024-04-13 15:49:04 +0500

    created TODO App

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let todo = [];
+let condition = true;
+while (condition) {
+    let todoquestion = await inquirer.prompt([
+        {
+            name: "Q1",
+            type: "input",
+            message: "What would you like to add in todos?",
+            //   default: "",
+        },
+        {
+            name: "Q2",
+            type: "confirm",
+            message: "would you like to add another one?",
+            default: true,
+        },
+    ]);
+    if (todoquestion.Q1 === "") {
+        console.log("please enter something");
+    }
+    else {
+        todo.push(todoquestion.Q1);
+        condition = todoquestion.Q2;
+        console.log(todo);
+    }
+}
